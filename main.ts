b1466ab22ac459a143bc70956625fd71f4c655f6
ZehMasterBeef <130101075+ZehMasterBeef@users.noreply.github.com>
2025-01-29 09:18:37 +0100

    end version

--- main.ts
+++ main.ts
@@ -1,26 +1,10 @@
-function Open_Gate () {
-    pins.servoWritePin(AnalogPin.P3, 90)
-    HasWaited = 0
-    while (HasWaited != 5) {
-        HasWaited += 1
-    }
-    Close_Gate()
-}
-function Close_Gate () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        pins.servoWritePin(AnalogPin.P3, 0)
-    }
-}
-let HasWaited = 0
 pins.servoWritePin(AnalogPin.P3, 0)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        Open_Gate()
+        pins.servoWritePin(AnalogPin.P3, 90)
+        basic.pause(5000)
     }
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        Open_Gate()
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        pins.servoWritePin(AnalogPin.P3, 0)
     }
-})
-basic.forever(function () {
-	
 })
